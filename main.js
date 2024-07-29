f39df1582873e8aa28f828e5353f58b814d1fe68
Balazs Molnar <molnarb@starschema.onmicrosoft.com>
2024-07-29 10:30:20 +0200

    Add author secttion

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 function readingTime() {
     const text = document.getElementById("article").innerText;
-    console.log(text);
     const wpm = 225;
     const words = text.trim().split(/\s+/).length;
-    console.log(words);
     const time = Math.ceil(words / wpm);
-    console.log(time);
     document.getElementById("reading-time").innerText = time;
 }
 readingTime();
 
-document.getElementById('search-input').addEventListener('input', function() {
+document.getElementById('search-input').addEventListener('input', function () {
     let searchTerm = this.value.toLowerCase();
     let article = document.getElementById('article');
     let paragraphs = article.getElementsByTagName('p');
@@ -23,7 +20,7 @@ document.getElementById('search-input').addEventListener('input', function() {
     // Remove previous highlights
     function removeHighlights() {
         let highlights = article.querySelectorAll('span.highlight-search');
-        highlights.forEach(function(highlight) {
+        highlights.forEach(function (highlight) {
             let parent = highlight.parentNode;
             parent.replaceChild(document.createTextNode(highlight.textContent), highlight);
             parent.normalize(); // Merge adjacent text nodes
@@ -41,3 +38,26 @@ document.getElementById('search-input').addEventListener('input', function() {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'GET'
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (json) {
+            const randomNumber = Math.floor(Math.random() * json.length);
+            const author = json[randomNumber];
+
+            console.log(json[randomNumber])
+
+            const authorName = document.getElementById('author_name');
+            const authorEmail = document.getElementById('author_email');
+            const authorPhone = document.getElementById('author_phone');
+            const authorCompany = document.getElementById('author_company');
+
+            authorName.innerText = author.name;
+            authorEmail.innerText = author.email;
+            authorEmail.href = `mailto:${author.email}`;
+            authorPhone.innerText = author.phone;
+            authorCompany.innerText = author.company.name;
+        });
+});
